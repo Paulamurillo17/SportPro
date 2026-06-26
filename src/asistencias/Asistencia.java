abde74fa5dd01a971b607a05783ea46ec93c2f03
dylan <dylangutierrezctdf@gmail.com>
2026-06-26 16:15:48 -0600

    Se crearon las clases asistencias y disciplina
    Se crearon sus debidos procesos

--- src/asistencias/Asistencia.java
+++ src/asistencias/Asistencia.java
@@ -3,11 +3,37 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package asistencias;
-import java.time.LocalDate;
-/**
- *
- * @author Student
- */
+import participante.Participante;
+
 public class Asistencia {
-   private 
+
+    private Participante participante;
+    private String estado;
+
+    public Asistencia(Participante participante, String estado) {
+        this.participante = participante;
+        this.estado = estado;
+    }
+
+    public Participante getParticipante() {
+        return participante;
+    }
+
+    public void setParticipante(Participante participante) {
+        this.participante = participante;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Participante: " + participante.getNombreCompleto());
+        System.out.println("Estado: " + estado);
+    }
+
 }
--- src/disciplina/Disciplina.java
+++ src/disciplina/Disciplina.java
@@ -3,11 +3,58 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package disciplina;
+import responsable.Responsable;
 
-/**
- *
- * @author Student
- */
 public class Disciplina {
-    
+
+    private String nombre;
+    private String categoria;
+    private int capacidadMaxima;
+    private Responsable responsable;
+
+    public Disciplina(String nombre, String categoria, int capacidadMaxima, Responsable responsable) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.capacidadMaxima = capacidadMaxima;
+        this.responsable = responsable;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+    public void setCapacidadMaxima(int capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+    }
+
+    public Responsable getResponsable() {
+        return responsable;
+    }
+
+    public void setResponsable(Responsable responsable) {
+        this.responsable = responsable;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Categoria: " + categoria);
+        System.out.println("Capacidad maxima: " + capacidadMaxima);
+    }
+
 }
