c3dc907cf5eb174d9533adfaecf7a0246d438aa7
dylan <dylangutierrezctdf@gmail.com>
2026-06-26 15:20:30 -0600

    Se creo la clase Asistencia
    Se creo la clase y se añadieron sus metodos constructores

---
+++ src/asistencias/Asistencia.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package asistencias;
+import java.time.LocalDate;
+/**
+ *
+ * @author Student
+ */
+public class Asistencia {
+   private 
+}
